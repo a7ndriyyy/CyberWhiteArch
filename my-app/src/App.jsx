229c6bfbaac1481a67e27d7726810e49df277eb2
Andrii Nastych <117773717+a7ndriyyy@users.noreply.github.com>
2025-01-27 13:36:58 +0100

    home correct

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 import HeaderComponent from './components/Header';
+import Home from './components/HomePage/Home';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+
+const RegistrationForm = () => <h1>Registration Form (Coming Soon)</h1>;
+
 
 function App() {
   return (
 
     <div className="App">
     <HeaderComponent></HeaderComponent>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegistrationForm />} />
+      </Routes>
+    </Router>
      
     </div>
   );
