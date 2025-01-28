0562756da0d8d052f05de11d9f02c898d5b2d291
Andrii Nastych <117773717+a7ndriyyy@users.noreply.github.com>
2025-01-28 10:05:35 +0100

    loader

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,23 +3,39 @@ import './App.css';
 import HeaderComponent from './components/Header';
 import Home from './components/HomePage/Home';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useEffect,useState } from 'react';
+import Loader from './components/Loader/Loader'
 
 
 const RegistrationForm = () => <h1>Registration Form (Coming Soon)</h1>;
 
 
 function App() {
-  return (
+  const [showLoader, setShowLoader] = useState(true); // Loader visibility state
 
+  useEffect(() => {
+    // Simulate loader duration
+    const timer = setTimeout(() => {
+      setShowLoader(false);
+    }, 5000); // 5 seconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
     <div className="App">
-    <HeaderComponent></HeaderComponent>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegistrationForm />} />
-      </Routes>
-    </Router>
-     
+      {showLoader ? (
+        <Loader /> // Show loader while `showLoader` is true
+      ) : (
+        <>
+          <HeaderComponent />
+          <Router>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<RegistrationForm />} />
+            </Routes>
+          </Router>
+        </>
+      )}
     </div>
   );
 }
