eef49de0d97a0585af61db840995187aa002cc3f
Andrii Nastych <117773717+a7ndriyyy@users.noreply.github.com>
2025-06-24 11:47:46 +0200

    community

---
+++ my-app/src/components/CommunityPage/sectionBanner/CyberBanner.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './CyberBanner.css';
+import bunnylogo from '../../../assets/bunnylogo.png'; // Adjust the path as necessary
+
+const CyberBanner = () => (
+  <section className="cyber-banner">
+    <div className="binary-strip">
+  <div className="binary-strip-container">
+    <div className='binary'>
+         <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    <span className='hover-binary'>1101001 1010101 1110001 0010110 1001010 1101010 1111000 </span>
+    </div>
+
+  </div>
+
+    </div>
+    <div className="cyber-message">
+        <div className='cyber-text'>
+     <p>If you are interested in your security on the internet, join our ranks of cyber warriors</p>
+      <div className="light-button">
+  <button className="bt">
+    <div className="light-holder">
+      <div className="dot"></div>
+      <div className="light"></div>
+    </div>
+    <div className="button-holder">
+      <p>TRY to HACK RIGHT NOW</p>
+    </div>
+  </button>
+</div>
+        </div>
+         <img src={bunnylogo} alt="bunylogo" />
+    </div>
+  </section>
+);
+
+export default CyberBanner;
--- my-app/src/pages/Community/Community.jsx
+++ my-app/src/pages/Community/Community.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import CommunityPage from '../../components/CommunityPage/CommunityPage';
 import ChatCard from '../../components/CommunityPage/sectionChat/chatSection';
 import PrivacyInfo from '../../components/CommunityPage/sectionBenefits/PrivacyInfo';
+import CyberBanner from '../../components/CommunityPage/sectionBanner/CyberBanner';
 
 const Community = () => {
   return (
@@ -10,6 +11,7 @@ const Community = () => {
         <CommunityPage />
         <ChatCard/>
         <PrivacyInfo/>
+        <CyberBanner/>
       </section>
     </main>
   );
